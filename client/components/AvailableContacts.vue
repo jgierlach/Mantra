1bf2765178ad235b145255d7c2548557a1e0951b
jgierlach <jegierlach@gmail.com>
2020-01-04 15:54:52 -0500

    Select a quote feature

---
+++ client/components/AvailableContacts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-4 text-center">
+    <h1>Available Contacts</h1>
+    <select>
+      <option
+        @click="addToContactSendList(contact)"
+        v-for="contact in contacts"
+        :key="contact._id"
+      >{{contact.name}}</option>
+    </select>
+
+    <div class="mt-4 text-center">
+      <ContactCard v-for="contact in contactSendList" :key="contact._id" :contact="contact" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactCard from './ContactCard.vue'
+
+export default {
+  computed: {
+    contacts() {
+      return this.$store.state.contacts
+    },
+    contactSendList() {
+      return this.$store.state.contactSendList
+    }
+  },
+  methods: {
+    addToContactSendList(contact) {
+      this.$store.commit('addToContactSendList', contact)
+    }
+  },
+  components: {
+    ContactCard
+  }
+}
+</script>
+
+<style>
+</style>
--- client/pages/Home.vue
+++ client/pages/Home.vue
@@ -3,15 +3,18 @@
     <h1 class="text-center text-brown" v-if="!auth">Mevn Auth</h1>
     <h1 class="text-center" v-else>Welcome, {{ user.name }}</h1>
     <GenerateQuote />
+    <AvailableContacts />
   </div>
 </template>
 
 <script>
 import GenerateQuote from '../components/GenerateQuote.vue'
+import AvailableContacts from '../components/AvailableContacts.vue'
 
 export default {
   components: {
-    GenerateQuote
+    GenerateQuote,
+    AvailableContacts
   }
 }
 </script>
